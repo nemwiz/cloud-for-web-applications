0149d955f0817acf0cde62de2c112bf5986946ae
neni <Nemanja.Ninkovic@zuehlke.com>
2022-09-29 14:06:09 +0800

    upgrades elasticbeanstalk to a newer AMI version

--- containers.ts
+++ containers.ts
@@ -108,7 +108,7 @@ export const createDockerContainerDeployment = (appName: string) => {
             name: `${appEnvironment}-${appName}`, // A unique name for this environment. This name is used in the application URL
             application: app.name,
             solutionStackName:
-                "64bit Amazon Linux 2 v3.4.17 running Docker",
+                "64bit Amazon Linux 2 v3.4.19 running Docker",
             settings: [
                 // "Modify Security" in the console
                 {
